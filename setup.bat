a04c5370cbb615a8d7aa49b1735285ee788456fa
Nick Sperry <nmcsperry@gmail.com>
2018-04-26 17:33:44 -0500

    made the setup script possibly work

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 
 pushd %CD%
-call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 popd
